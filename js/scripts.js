2ef580032f35936692626f39de6663bffc70d9df
Corey Miller <coreyMiller353@gmail.com>
2019-10-11 10:20:09 -0400

    create card elements for each person

--- js/scripts.js
+++ js/scripts.js
@@ -2,26 +2,34 @@ const gallery = document.getElementById('gallery');
 
 
 async function fetchData() {
-    const res = await fetch('https://randomuser.me/api/?results=50');
+    const res = await fetch('https://randomuser.me/api/?results=12');
     const data = await res.json();
+    return data
+};
+
+
+function createCard(data) {
     data.results.forEach((person) => {
-        let html = "" 
-        html += `<img src="${person.picture.thumbnail}">${person.name.first} ${person.name.last}<br>`
-        gallery.innerHTML += html;
-        });
+        const card = document.createElement('div');
+        const cardImg = document.createElement('div');
+        const cardInfo = document.createElement('div');
+        card.className = ('card');
+        cardImg.className = ('card-img-container');
+        cardInfo.className = ('card-info-container');
+        gallery.appendChild(card);
+        card.appendChild(cardImg);
+        card.appendChild(cardInfo);
+        console.log(person)
+        cardImg.innerHTML = `
+        <img class="card-img" src="${person.picture.large}" alt="profile picture">
+        `
+        cardInfo.innerHTML = `
+        <h3 id="name" class="card-name cap">${person.name.first} ${person.name.last}</h3>
+                        <p class="card-text">${person.email}</p>
+                        <p class="card-text cap">${person.location.city}, ${person.location.state}</p>
+        `
     }
+)};
 
-fetchData();
-
-
-// .then(res => res.json())
-            // .then(data => {
-            //     console.log(data)
-            //     return data
-            // })
-            // .then(data => {
-            //     const person = data.results[0];
-            //     gallery.innerHTML = `<img src="${person.picture.thumbnail}"> <span> ${person.name.first} ${person.name.last} </span>`;
-            // })
-            // .catch(error => Error(console.log("There was an error")));
+fetchData().then(data => createCard(data));
 
