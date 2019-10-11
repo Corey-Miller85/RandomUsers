61c443102dbf9f5d5e871407bac39add50332dc8
Corey Miller <coreyMiller353@gmail.com>
2019-10-11 02:32:29 -0400

    change code to async/await

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,24 @@
 const gallery = document.getElementById('gallery');
 
 
-fetch('https://randomuser.me/api/')
-            .then(res => res.json())
-            .then(data => {
-                console.log(data)
-                return data
-            })
-            .then(data => {
-                const person = data.results[0];
-                gallery.innerHTML = `<img src="${person.picture.thumbnail}"> <span> ${person.name.first} ${person.name.last} </span>`;
-            })
-            .catch(error => Error(console.log("There was an error")));
+async function fetchData() {
+    const res = await fetch('https://randomuser.me/api/');
+    const data = await res.json();
+    const person = data.results[0];
+    gallery.innerHTML = `<img src="${person.picture.thumbnail}"> <span> ${person.name.first} ${person.name.last} </span>`;
+}
+
+fetchData();
+
+
+// .then(res => res.json())
+            // .then(data => {
+            //     console.log(data)
+            //     return data
+            // })
+            // .then(data => {
+            //     const person = data.results[0];
+            //     gallery.innerHTML = `<img src="${person.picture.thumbnail}"> <span> ${person.name.first} ${person.name.last} </span>`;
+            // })
+            // .catch(error => Error(console.log("There was an error")));
 
