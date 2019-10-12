04f1c214ee71a438bdabebef93b187450971bd55
Corey Miller <coreyMiller353@gmail.com>
2019-10-11 23:08:50 -0400

    start propogating modal info

--- js/scripts.js
+++ js/scripts.js
@@ -32,13 +32,16 @@ function createCard(data) {
         card.appendChild(info);
 
         card.addEventListener ('click', (event) => {
+            console.log(event.target)
             if (event.target.parentNode.className != "gallery"){
+                const personNameParent = event.target.parentNode.querySelector('#name').innerHTML
+                populateModal(personNameParent);
                 document.querySelector(".modal-container").style.display = 'block';
-                console.log(event.target.parentNode.querySelector('#name').innerHTML);
+                
             } else {
+                const personNameNoParent = event.target.querySelector('#name').innerHTML;
+                populateModal(personNameNoParent);
                 document.querySelector(".modal-container").style.display = 'block';
-                console.log(event.target.querySelector('#name').innerHTML);
-
             }
         });
 
@@ -54,23 +57,30 @@ function createModal() {
     const createDiv = document.createElement('div');
     createDiv.classList.add('modal')
     modalDiv.appendChild(createDiv);
-    createDiv.innerHTML = `
-        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-        <div class="modal-info-container">
-        <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
-        <h3 id="name" class="modal-name cap">name</h3>
-        <p class="modal-text">email</p>
-        <p class="modal-text cap">city</p>
-        <hr>
-        <p class="modal-text">(555) 555-5555</p>
-        <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-        <p class="modal-text">Birthday: 10/21/2015</p>
-    `;
     modalDiv.style.display = "none";
-
+    
     modalDiv.addEventListener('click', (e) => {
         modalDiv.style.display = "none";
     });
 }
 
 createModal();
+
+function populateModal(name) { 
+    jsonData.forEach((person) => {
+        if (person.name.first + " " + person.name.last == name) {
+            document.querySelector('.modal').innerHTML = `
+                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                <div class="modal-info-container">
+                <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
+                <h3 id="name" class="modal-name cap">${person.name.first} ${person.name.last}</h3>
+                <p class="modal-text">${person.email}</p>
+                <p class="modal-text cap">${person.location.city}</p>
+                <hr>
+                <p class="modal-text">(555) 555-5555</p>
+                <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+                <p class="modal-text">Birthday: 10/21/2015</p>
+            `;
+        }
+    });
+}
